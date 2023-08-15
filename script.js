0d5900f1324e5e79c7214a5f88d8a74ae09f903b
Surya Narayanan <suryanarayanan7798@gmail.com>
2023-08-15 13:59:10 +0530

    Update script.js
    Third commit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const person = {
     name: "Surya",
-    occupation: "Product Designer"
+    occupation: "Product Designer",
+    city: "Delhi"
 }
 
 for (let i = 0; i < 5; i++) {
-    console.log(`Hi, I'm ${person.name}, a ${person.occupation}.`)    
+    console.log(`Hi, I'm ${person.name}, a ${person.occupation} based out of ${person.city}.`)    
 }
